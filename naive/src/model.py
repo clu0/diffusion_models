37e5a84fe517c75cf21d16984206ef72bff130cb
clu0 <chenlu96@gmail.com>
2024-01-29 17:16:27 +0000

    wip logging

--- naive/src/model.py
+++ naive/src/model.py
@@ -29,6 +29,7 @@ class Resnet(nn.Module):
         name: str,
         n_group_norm: int = 4,
         embd_dim: int = EMBEDDING_DIM,
+        classifier_free: bool = False,
     ):
         super().__init__()
         self.name = name
@@ -47,6 +48,12 @@ class Resnet(nn.Module):
             nn.GroupNorm(n_group_norm, embd_dim),
             nn.Linear(embd_dim, c_out),
         )
+        self.classifier_free = classifier_free
+        if self.classifier_free:
+            self.class_block = nn.Sequential(
+                nn.GroupNorm(n_group_norm, embd_dim),
+                nn.Linear(embd_dim, c_out),
+            )
         self.post_timestep_block = nn.Sequential(
             nn.GroupNorm(n_group_norm, c_out),
             nn.SiLU(),
@@ -56,12 +63,17 @@ class Resnet(nn.Module):
         if c_in != c_out:
             self.identity_conv = nn.Conv2d(c_in, c_out, 1)
 
-    def forward(self, x: torch.Tensor, t_emb: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, t_emb: torch.Tensor, c_emb: Optional[torch.Tensor] = None) -> torch.Tensor:
         """Forward pass"""
         logger.info(f"Resnet {self.name} input: x={x.size()}, t={t_emb.size()}, c_in={self.c_in}, c_out={self.c_out}")
         h: torch.Tensor = self.pre_timestep_block(x)
         t_emb = self.timestep_block(t_emb)
         h = h + t_emb[:, :, None, None]
+        logger.info(f"t_emb={t_emb.size()}")
+        if c_emb is not None:
+            c_emb = self.class_block(c_emb)
+            h += c_emb[:, :, None, None]
+            logger.info(f"Resnet {self.name} c_emb={c_emb.size()}")
         logger.info(f"Resnet {self.name} pre timestep: h={h.size()}, t={t_emb.size()}")
         h = self.post_timestep_block(h)
         if hasattr(self, "identity_conv"):
@@ -78,7 +90,7 @@ class Downsample(nn.Module):
         self.name = name
         self.conv = nn.Conv2d(c_in, c_out, 3, stride=2, padding=1)
 
-    def forward(self, x: torch.Tensor, t_emb: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, t_emb: torch.Tensor, c_emb: Optional[torch.Tensor] = None) -> torch.Tensor:
         out: torch.Tensor = self.conv(x)
         assert out.size(2) == x.size(2) // 2
         assert out.size(3) == x.size(3) // 2
@@ -94,7 +106,7 @@ class Upsample(nn.Module):
         self.name = name
         self.conv = nn.Conv2d(c_in, c_out, 3, padding=1)
 
-    def forward(self, x: torch.Tensor, t_emb: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, t_emb: torch.Tensor, c_emb: Optional[torch.Tensor] = None) -> torch.Tensor:
         out: torch.Tensor = F.interpolate(x, scale_factor=2)
         out = self.conv(out)
         assert out.size(2) == x.size(2) * 2
@@ -104,7 +116,7 @@ class Upsample(nn.Module):
 
 
 def get_embedding(
-    num_embeddings: int, embedding_dim: int, padding_idx: Optional[int] = None
+    num_embeddings: int, embedding_dim: int, padding_idx: Optional[int] = None, null_token: bool = False
 ):
     """Build sinusoidal embeddings.
 
@@ -125,6 +137,9 @@ def get_embedding(
         emb = torch.cat([emb, torch.zeros(num_embeddings, 1)], dim=1)
     if padding_idx is not None:
         emb[padding_idx, :] = 0
+    if null_token:
+        # add another row of zeros
+        emb = torch.cat([emb, torch.zeros(1, embedding_dim)], dim=0)
     return emb
 
 
@@ -140,13 +155,21 @@ class Unet(nn.Module):
         c_between: List[int] = [16, 32, 64, 128],
         n_group_norm: int = 4,
         embd_dim: int = EMBEDDING_DIM,
+        classifier_free: bool = False,
+        n_classes: int = 0,
     ):
         super().__init__()
         self.name = "Unet"
+        self.classifier_free = classifier_free
 
         self.register_buffer(
             "timestep_embedding", get_embedding(1000, embd_dim), persistent=False
         )
+        if classifier_free:
+            self.register_buffer(
+                "classifier_embedding", get_embedding(n_classes, embd_dim, null_token=True), persistent=False
+            )
+            logger.info(f"Classifier embedding: {self.classifier_embedding.size()}")
 
         self.learnable_embedding_block = nn.Sequential(
             nn.Linear(embd_dim, embd_dim),
@@ -166,6 +189,7 @@ class Unet(nn.Module):
                     name=f"res_down_{i}",
                     n_group_norm=n_group_norm,
                     embd_dim=embd_dim,
+                    classifier_free=self.classifier_free,
                 )
             )
             self.downblocks.append(
@@ -178,6 +202,7 @@ class Unet(nn.Module):
                 name="res_middle_1",
                 n_group_norm=n_group_norm,
                 embd_dim=embd_dim,
+                classifier_free=self.classifier_free,
             )
         )
 
@@ -192,6 +217,7 @@ class Unet(nn.Module):
                 name="res_middle_2",
                 n_group_norm=n_group_norm,
                 embd_dim=embd_dim,
+                classifier_free=self.classifier_free,
             )
         )
         reversed_c = list(reversed(c_between))
@@ -205,6 +231,7 @@ class Unet(nn.Module):
                     name=f"res_up_{i}",
                     n_group_norm=n_group_norm,
                     embd_dim=embd_dim,
+                    classifier_free=self.classifier_free,
                 )
             )
             self.upblocks.append(Upsample(c_in, c_out, name=f"up_{i}"))
@@ -215,12 +242,19 @@ class Unet(nn.Module):
             name="res_output",
             n_group_norm=n_group_norm,
             embd_dim=embd_dim,
+            classifier_free=self.classifier_free,
         )
         self.output_conv = nn.Conv2d(c_last, c_start, 3, padding=1)
 
-    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, t: torch.Tensor, c: Optional[torch.Tensor] = None) -> torch.Tensor:
         # get timestep embeddings
         t_emb: torch.Tensor = self.timestep_embedding
+        c_emb: Optional[torch.Tensor] = None
+        if self.classifier_free:
+            assert c is not None
+            c_emb: torch.Tensor = self.classifier_embedding
+            c_emb = c_emb.index_select(0, c.view(-1)).view(c.size(0), -1)
+            logger.info(f"c_emb={c_emb.size()}")
 
         # the next step is copying the DDPM implementation, which passes the timestep embeddings through some dense layers
         # But this is actually quite weird, because if we're passing time indices through a randomly initialized dense layer anyway,
@@ -230,7 +264,7 @@ class Unet(nn.Module):
 
         x = self.initial_conv(x)
         for module in self.downblocks:
-            x = module(x, t_emb)
+            x = module(x, t_emb, c_emb)
         logger.info(f"x={x.size()}, t_emb={t_emb.size()}")
         x_embedded = x.view(x.size(0), x.size(1), -1) # (B, C, H*W)
         x_embedded = x_embedded.transpose(1, 2) # (B, H*W, C)
@@ -240,7 +274,7 @@ class Unet(nn.Module):
         logger.info(f"h={h.size()}")
         x = x + h
         for module in self.upblocks:
-            x = module(x, t_emb)
-        x = self.output_res(x, t_emb)
+            x = module(x, t_emb, c_emb)
+        x = self.output_res(x, t_emb, c_emb)
         x = self.output_conv(x)
         return x
--- naive/train.py
+++ naive/train.py
@@ -5,6 +5,11 @@ TODO: substitute the model for an actual UNet
 """
 from typing import Tuple, Optional
 from time import time
+import argparse
+import random
+import sys
+import os
+import datetime
 
 import torch
 import torch.nn as nn
@@ -15,8 +20,10 @@ from torch.utils.data import DataLoader
 
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 
 from src.model import Unet
+from src.logger import Logger, HumanOutputFormat, CSVOutputFormat
 
 
 T: int = 1000
@@ -27,12 +34,6 @@ in_c: int = 1
 in_h: int = 32
 in_w: int = 32
 
-lr: float = 6 * 1e-4
-n_epoch: int = 280
-batch_size: int = 64
-
-past_model_pth: Optional[str] = "models/model_epoch_220.pth"
-past_n_epoch = 220
 
 device = torch.device("cpu")
 if torch.cuda.is_available():
@@ -50,11 +51,20 @@ alphas = alphas.to(device=device)
 
 
 class DiffusionTrainingDataset(Dataset):
-    def __init__(self, data: torch.Tensor):
+    def __init__(
+        self,
+        data: torch.Tensor,
+        classes: npt.NDArray[np.int_],
+        n_classes: int,
+        p_null: Optional[float] = None,
+    ):
         # assume that the input data has already been preprocessed into shape (N, in_c, in_h, in_w)
         assert data.ndim == 4 and data.shape[1:] == (in_c, in_h, in_w)
         # using mnist right now, can just load the whole dataset to cuda
         self.data = data.to(device=device)
+        self.classes = classes
+        self.n_classes = n_classes
+        self.p_null = p_null
 
     def __len__(self):
         return len(self.data)
@@ -65,60 +75,110 @@ class DiffusionTrainingDataset(Dataset):
         """
         return the i-th data, along with a random normal of same shape, and a random time step in {1, ..., T}
         """
+        if self.p_null is not None and random.random() < self.p_null:
+            # with some probability, train with null class token
+            t = torch.tensor([self.n_classes], device=device, dtype=torch.long)
+        else:
+            t = torch.randint(0, T, (1,), device=device, dtype=torch.long)
         return (
             self.data[idx, ...],
             torch.randn((in_c, in_h, in_w), device=device, dtype=torch.float32),
-            torch.randint(0, T, (1,), device=device, dtype=torch.int32),
+            t,
+            torch.tensor([self.classes[idx]], device=device, dtype=torch.long),
         )
 
 
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_epoch", type=int, default=500, help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=128, help="batch size")
+    parser.add_argument("--lr", type=float, default=6*1e-4, help="learning rate")
+    parser.add_argument("--p_null", type=float, default=0.1, help="probability of null class token")
+    parser.add_argument("--model_checkpoint", type=str, default=None, help="path to past model")
+    parser.add_argument("--past_n_epoch", type=int, default=None, help="epoch of past model")
+    parser.add_argument("--data_tensor_path", type=str, default="mnist_upsampled.pt", help="path to data tensor")
+    parser.add_argument("--metadata_path", type=str, default="mnist/mnist.csv", help="path to metadata")
+    parser.add_argument("--model_save_dir", type=str, default="models/", help="prefix for saving model")
+    parser.add_argument("--model_save_prefix", type=str, default="")
+    parser.add_argument("--log_save_suffix", type=str, default="")
+    parser.add_argument("--log_save_dir", type=str, default="logs/", help="prefix for saving model")
+    parser.add_argument("--n_classes", type=int, default=10, help="number of classes")
+    parser.add_argument("--classifier_free", dest='classifier_free', action='store_true', help="use classifier free training")
+    parser.set_defaults(classifier_free=False)
+    return parser
+
+
 if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    print(f"training args: \n{args}")
+
     # Train with mnist
-    mnist_upsampled: torch.Tensor = torch.load("mnist_upsampled.pt")
+    mnist_upsampled: torch.Tensor = torch.load(args.data_tensor_path)
     mnist_upsampled = mnist_upsampled.float()
+    mnist_metadata = pd.read_csv(args.metadata_path)
+    mnist_classes = mnist_metadata["label"].to_numpy()
+    p_null = None
+    if args.classifier_free:
+        print(f"training with classifier free")
+        p_null = args.p_null
     train_dataloader = DataLoader(
-        DiffusionTrainingDataset(mnist_upsampled),
-        batch_size=batch_size,
+        DiffusionTrainingDataset(mnist_upsampled, mnist_classes, n_classes=args.n_classes, p_null=p_null),
+        batch_size=args.batch_size,
         shuffle=True,
     )
 
-    model = Unet(c_start=in_c)
-    if past_model_pth is not None:
-        model.load_state_dict(torch.load(past_model_pth))
+    model = Unet(c_start=in_c, classifier_free=args.classifier_free, n_classes=args.n_classes)
+    if args.model_checkpoint is not None:
+        model.load_state_dict(torch.load(args.model_checkpoint))
     model.to(device=device)
-    print(f"loaded model {past_model_pth}")
+    print(f"loaded model {args.model_checkpoint}")
 
     loss_fn = F.mse_loss
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     train_losses = []
+    epoch_losses = []
 
     model.train()
-    print(f"starting training for {n_epoch} epochs")
-    for i in range(n_epoch):
+    print(f"starting training for {args.n_epoch} epochs")
+
+    # initialize save dir and logger
+    log_save_dir = os.path.join(args.log_save_dir, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    os.mkdir(log_save_dir, exist_ok=True)
+    model_save_dir = os.path.join(args.model_save_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    os.mkdir(model_save_dir, exist_ok=True)
+    output_formats = [
+        HumanOutputFormat(sys.stdout),
+        HumanOutputFormat(os.path.join(log_save_dir, f"log{args.log_save_suffix}.txt")),
+        CSVOutputFormat(os.path.join(log_save_dir, f"progress{args.log_save_suffix}.csv"))
+    ]
+    logger = Logger(output_formats)
+    
+    for i in range(args.n_epoch):
         epoch_start = time()
-        i += past_n_epoch
+        if args.past_n_epoch is not None:
+            i += args.past_n_epoch
+
+        logger.logkv("epoch", i)
         for j, batch in enumerate(train_dataloader):
             if (j + 1) % 100 == 0:
                 print(f"epoch {i}, batch {j}, loss {np.mean(train_losses[-100:])}")
-            data, noise, times = batch
-            #data, noise, times = (
-            #    torch.from_numpy(data),
-            #    torch.from_numpy(noise),
-            #    torch.from_numpy(times),
-            #)
-            #data, noise, times = data.to(device), noise.to(device), times.to(device)
+            data, noise, times, classes = batch
+            if not args.classifier_free:
+                classes = None
             optimizer.zero_grad()
             alpha_ts = alphas[times]
             alpha_ts = alpha_ts.view(alpha_ts.size(0), alpha_ts.size(1), 1, 1)
             x_ts = (torch.sqrt(alpha_ts) * data + torch.sqrt(1 - alpha_ts) * noise).float()
-            pred = model(x_ts, times)
+            pred = model(x_ts, times, classes)
             loss_vals = loss_fn(pred, noise)
             loss_vals.backward()
             optimizer.step()
 
             train_losses.append(loss_vals.item())
         print(f"finished epoch {i}, took {time() - epoch_start} seconds")
-        np.save("losses/train_losses_220-500.npy", np.array(train_losses))
+        np.save(f"{args.loss_save_prefix}_{i+1}.npy", np.array(train_losses))
         if (i + 1) % 20 == 0:
-            torch.save(model.state_dict(), f"models/model_epoch_{i + 1}.pth")
+            torch.save(model.state_dict(), f"{args.model_save_prefix}_epoch_{i + 1}.pth")
 
